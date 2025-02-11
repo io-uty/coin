ac166489500ff394cf93f673f6affb01bd92e3a5
io-uty <hyparksj@inha.edu>
2025-02-12 00:19:17 +0900

    지정가 매수/매도

--- login.py
+++ login.py
@@ -1,4 +1,5 @@
 import pyupbit
+import pprint
 
 # upbit.txt에서 API 키 읽기
 f = open("upbit.txt")
@@ -10,12 +11,16 @@ f.close()
 # Upbit 클래스 인스턴스 생성
 upbit = pyupbit.Upbit(access, secret)
 
+balances = upbit.get_balances()
 # 개별 잔액 조회
-balance_krw = upbit.get_balance("KRW")
-balance_btc = upbit.get_balance("KRW-BTC")
-balance_eth = upbit.get_balance("KRW-ETH")
+# balance_krw = upbit.get_balance("KRW")
+# balance_btc = upbit.get_balance("KRW-BTC")
+# balance_eth = upbit.get_balance("KRW-ETH")
 
 # 결과 출력
-print("KRW 잔액:", balance_krw)
-print("BTC 잔액:", balance_btc)
-print("ETH 잔액:", balance_eth)
+# print("KRW 잔액:", balance_krw)
+# print("BTC 잔액:", balance_btc)
+# print("ETH 잔액:", balance_eth)
+
+pprint.pprint(balances[0])
+  
